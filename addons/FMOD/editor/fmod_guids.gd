037e239fd743ac0ff47014753cbda210e4563acd
ElporoKing <baptiste.denoux@gmail.com>
2024-01-22 19:15:15 +0100

    Create fmod_guids.gd

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,18 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'GGJ.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const MUSIC = "{2ad4f55c-ac06-437d-af47-59957ea065c2}"
+
+class Busses:
+	const MASTER_BUS = "{3c54403a-2287-45b3-a989-e6f01d060923}"
+	const REVERB = "{28e85613-4496-4534-8344-9c2e68795a59}"
+
+class Banks:
+	const MASTER = "{fcf57c1d-90c8-4561-8e67-14a8c8a19aff}"
+
